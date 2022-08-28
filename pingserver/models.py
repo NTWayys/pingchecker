fad7af20983feb9bf4c8b2ed10eb665a91814c4a
Wayys <ntwayys@gmail.com>
2022-08-28 15:36:51 +0200

    added date added to the server

--- pingserver/models.py
+++ pingserver/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django_q.models import Schedule
+
 # Create your models here.
 
 
@@ -9,12 +9,6 @@ class server(models.Model):
         User, on_delete=models.SET_NULL, null=True)
     address = models.CharField(max_length=100, default="8.8.8.8")
     status = models.CharField(max_length=100, default="")
-    notes = models.JSONField(default="Server was last down never")
-
-
-Schedule.objects.create(
-    func='pingserver.utils.updateServerStatus',  # module and func to run
-    schedule_type=Schedule.MINUTES,
-    minutes=1,  # run every minute
-    repeats=-1  # keep repeating, repeat forever
-)
+    notes = models.JSONField(default="Server was last down never;")
+    dateadded = models.DateTimeField(
+        auto_now_add=True, blank=True)
--- pingserver/utils.py
+++ pingserver/utils.py
@@ -5,30 +5,18 @@ import os
 import requests
 from datetime import datetime
 from time import sleep
+from .config import siteConfigData
+siteURL = siteConfigData['url']
 
 
-def getServerList(request):
-    items = server.objects.all()
-    serializer = serverListSerializer(items, many=True)
-    return Response(serializer.data)
-
-
-def getServerDetail(request, id):
-    items = server.objects.get(id=id)
-    serializer = serverListSerializer(items, many=False)
-    return Response(serializer.data)
-
-
-def createServer(request, domain):
-    print("createblock")
+def createServer(domain):
     serializer = serverListSerializer(data={'address': domain}, partial=True)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
 
 
-def updateServer(request, id):
-    data = request.data
+def updateServer(data, id):
     item = server.objects.get(id=id)
     serializer = serverListSerializer(instance=item, data=data, partial=True)
 
@@ -38,83 +26,75 @@ def updateServer(request, id):
     return Response(serializer.data)
 
 
-def deleteServer(request, id):
-    item = server.objects.get(id=id)
-    item.delete()
-    return Response('Server was Removed!')
-
-
-def frontPage(request):
-    updateServerStatus()
-    serverList = server.objects.all()
-    context = []
-    for domain in serverList:
-        serverInfo = {'address': domain.address,
-                      'status': domain.status,
-                      'notes': domain.notes.split(";")[0],
-                      }
-        context.append(serverInfo)
-    return context
-
-
-def domainTarget(request, domain):
+def domainTarget(domain):
     context = {}
     try:
         updateTargetDomain(domain)
-        domain = server.objects.get(address=domain)
-        serverInfo = {'address': domain.address,
-                      'status': domain.status,
-                      'notes': domain.notes.split(";")[0],
+        targetDomain = server.objects.get(address=domain)
+        serverInfo = {'dateadded': targetDomain.dateadded,
+                      'address': targetDomain.address,
+                      'status': targetDomain.status,
+                      'notes': targetDomain.notes.split(";")[0],
                       }
         context = serverInfo
     except:
-        createServer(request, domain)
-        updateServerStatus()
-        print("one")
-        domain = server.objects.get(address=domain)
-        serverInfo = {'address': domain.address,
-                      'status': domain.status,
-                      'notes': domain.notes.split(";")[0],
+        createServer(domain)
+        updateTargetDomain(domain)
+        targetDomain = server.objects.get(address=domain)
+        serverInfo = {'dateadded': "This is the first time this domain has been added to the server therefor it won't have info prior to today",
+                      'address': targetDomain.address,
+                      'status': targetDomain.status,
+                      'notes': targetDomain.notes.split(";")[0],
                       }
         context = serverInfo
     return context
 
 
 def updateTargetDomain(domain):
-    domainTarget = server.objects.get(address=domain)
-    context = []
-    osrequest = os.popen("ping -n 1 " + domainTarget.address).read()
-    if "Destination host unreachable" in osrequest:
-        reply = domainTarget.address + " is unreachable"
-        requests.put('http://192.168.1.54:8000/server/' + str(domainTarget.id) + '/update/',
-                     json={"status":  reply, "notes":  "Server was last down at " + str(datetime.now().replace(microsecond=0)) + " ; "+domainTarget.notes})
-    elif "TTL=" in osrequest:
+    targetDomain = server.objects.get(address=domain)
+    osrequest = os.popen("ping -n 1 " + targetDomain.address).read()
+    if "TTL=" in osrequest:
         for sentence in osrequest.split(":"):
             if 'Average' in sentence:
-                reply = domainTarget.address + \
+                reply = targetDomain.address + \
                     " is online, " + sentence.strip("\n").strip()
-                updateServer()
-                requests.put('http://192.168.1.54:8000/server/' + str(domainTarget.id) + '/update/',
-                             json={"status":  reply})
+                updateServer({"status":  reply}, targetDomain.id)
+    elif "Destination host unreachable" in osrequest:
+        reply = targetDomain.address + " is unreachable"
+        updateServer({"status":  reply, "notes":  "Server was last down at " + str(
+            datetime.now().replace(microsecond=0)) + " ; "+targetDomain.notes}, targetDomain.id)
     elif "could not find host" in osrequest:
         reply = osrequest
-        requests.put('http://192.168.1.54:8000/server/' + str(domainTarget.id) + '/update/',
-                     json={"status":  reply})
+        updateServer({"status":  reply, "notes":  "Server was last down at " + str(
+            datetime.now().replace(microsecond=0)) + " ; "+targetDomain.notes}, targetDomain.id)
+    elif "Request timed out" in osrequest:
+        reply = osrequest
+        updateServer({"status":  reply, "notes":  "Server was last down at " + str(
+            datetime.now().replace(microsecond=0)) + " ; "+targetDomain.notes}, targetDomain.id)
 
 
-# this runs on a timer in models.py, reason for it being written a 2nd time is so I don't have to run non existing domains such as wayys.co.zsa
+# this runs on a timer of 1 min, using from django_q.models import Schedule
+
+
 def updateServerStatus():
     serverList = server.objects.all()
-    for domain in serverList:
-        osrequest = os.popen("ping -n 1 " + domain.address).read()
-        if "Destination host unreachable" in osrequest:
-            reply = domain.address + " is unreachable"
-            requests.put('http://192.168.1.54:8000/server/' + str(domain.id) + '/update/',
-                         json={"status":  reply, "notes":  "Server was last down at " + str(datetime.now().replace(microsecond=0)) + " ; "+domain.notes})
-        elif "TTL=" in osrequest:
+    for targetDomain in serverList:
+        osrequest = os.popen("ping -n 1 " + targetDomain.address).read()
+        if "TTL=" in osrequest:
             for sentence in osrequest.split(":"):
                 if 'Average' in sentence:
-                    reply = domain.address + \
+                    reply = targetDomain.address + \
                         " is online, " + sentence.strip("\n").strip()
-                    requests.put('http://192.168.1.54:8000/server/' + str(domain.id) + '/update/',
-                                 json={"status":  reply})
+                    updateServer({"status":  reply}, targetDomain.id)
+        elif "Destination host unreachable" in osrequest:
+            reply = targetDomain.address + " is unreachable"
+            updateServer({"status":  reply, "notes":  "Server was last down at " + str(
+                datetime.now().replace(microsecond=0)) + " ; "+targetDomain.notes}, targetDomain.id)
+        elif "could not find host" in osrequest:
+            reply = osrequest
+            updateServer({"status":  reply, "notes":  "Server was last down at " + str(
+                datetime.now().replace(microsecond=0)) + " ; "+targetDomain.notes}, targetDomain.id)
+        elif "Request timed out" in osrequest:
+            reply = osrequest
+            updateServer({"status":  reply, "notes":  "Server was last down at " + str(
+                datetime.now().replace(microsecond=0)) + " ; "+targetDomain.notes}, targetDomain.id)
